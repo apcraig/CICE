987af105e1c77cf2cb355e66e8a81a5bb46fb957
David A. Hebert <david.hebert@nrlssc.navy.mil>
2021-11-17 22:33:45 +0000

    Added more box tests to gridsys_suite.ts. Modified several set_nml for use with new islands grid and uniform forcing

--- configuration/scripts/tests/gridsys_suite.ts
+++ configuration/scripts/tests/gridsys_suite.ts
@@ -6,6 +6,14 @@ smoke          gbox80  1x1        boxslotcyl
 smoke	       gbox80  1x1        boxsymn
 smoke	       gbox80  1x1        boxsyme
 smoke	       gbox80  1x1        boxsymne
+smoke	       gbox80  1x1        boxsymn,kmtislands
+smoke	       gbox80  1x1        boxsyme,kmtislands
+smoke	       gbox80  1x1        boxsymne,kmtislands
+smoke	       gbox80  1x1        boxislands_n
+smoke	       gbox80  1x1        boxislands_e
+smoke	       gbox80  1x1        boxislands_ne
+smoke	       gbox80  1x1        boxadv
+
 
 smoke          gx3     8x2        diag1,run5day,gridcd
 restart        gx3     4x2        debug,diag1,gridcd
@@ -14,3 +22,10 @@ smoke          gbox80  1x1        boxslotcyl,gridcd
 smoke	       gbox80  1x1        boxsymn,gridcd
 smoke	       gbox80  1x1        boxsyme,gridcd
 smoke	       gbox80  1x1        boxsymne,gridcd
+smoke	       gbox80  1x1        boxsymn,kmtislands,gridcd
+smoke	       gbox80  1x1        boxsyme,kmtislands,gridcd
+smoke	       gbox80  1x1        boxsymne,kmtislands,gridcd
+smoke	       gbox80  1x1        boxislands_n,gridcd
+smoke	       gbox80  1x1        boxislands_e,gridcd
+smoke	       gbox80  1x1        boxislands_ne,gridcd
+smoke	       gbox80  1x1        boxadv,gridcd
