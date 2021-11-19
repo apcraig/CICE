d17e336f6e49d01b297289297eb024775255dff9
Philippe Blain <philippe.blain@canada.ca>
2021-11-19 10:18:29 -0500

    ice_dyn_shared: generalize 'principal_stress' arguments names
    In a subsequent commit we will call 'principal_stress' with the CD-grid
    stress arrays 'stress{p,m,12}T' to compute the principal stresses at the
    tracer point when using the CD grid.
    
    In that light, remove the '_1' suffix from the stress arguments since
    they won't always be located at the NE corner anymore.

--- cicecore/cicedynB/dynamics/ice_dyn_shared.F90
+++ cicecore/cicedynB/dynamics/ice_dyn_shared.F90
@@ -1352,13 +1352,13 @@
 !=======================================================================
 
 ! Computes principal stresses for comparison with the theoretical
-! yield curve; northeast values
+! yield curve
 !
 ! author: Elizabeth C. Hunke, LANL
 
       subroutine principal_stress(nx_block,   ny_block,  &
-                                  stressp_1,  stressm_1, &
-                                  stress12_1, strength,  &
+                                  stressp,    stressm,   &
+                                  stress12,   strength,  &
                                   sig1,       sig2,      &
                                   sigP)
 
@@ -1366,9 +1366,9 @@
          nx_block, ny_block  ! block dimensions
 
       real (kind=dbl_kind), dimension (nx_block,ny_block), intent(in) :: &
-         stressp_1 , & ! sigma11 + sigma22
-         stressm_1 , & ! sigma11 - sigma22
-         stress12_1, & ! sigma12
+         stressp   , & ! sigma11 + sigma22
+         stressm   , & ! sigma11 - sigma22
+         stress12  , & ! sigma12
          strength      ! for normalization of sig1 and sig2
 
       real (kind=dbl_kind), dimension (nx_block,ny_block), intent(out):: &
@@ -1393,14 +1393,14 @@
       do i = 1, nx_block
          if (strength(i,j) > puny) then
             ! ice internal pressure          
-            sigP(i,j) = -p5*stressp_1(i,j) 
+            sigP(i,j) = -p5*stressp(i,j) 
             
             ! normalized principal stresses
-            sig1(i,j) = (p5*(stressp_1(i,j) &
-                      + sqrt(stressm_1(i,j)**2+c4*stress12_1(i,j)**2))) &
+            sig1(i,j) = (p5*(stressp(i,j) &
+                      + sqrt(stressm(i,j)**2+c4*stress12(i,j)**2))) &
                       / strength(i,j)
-            sig2(i,j) = (p5*(stressp_1(i,j) &
-                      - sqrt(stressm_1(i,j)**2+c4*stress12_1(i,j)**2))) &
+            sig2(i,j) = (p5*(stressp(i,j) &
+                      - sqrt(stressm(i,j)**2+c4*stress12(i,j)**2))) &
                       / strength(i,j)         
          else
             sig1(i,j) = spval_dbl
