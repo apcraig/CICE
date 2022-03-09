554aece3b63df8285afa9f8ef8a9d8ca2ea24900
apcraig <anthony.p.craig@gmail.com>
2022-03-09 10:14:50 -0700

    fix OpenMP private variables on new code

--- cicecore/shared/ice_restart_column.F90
+++ cicecore/shared/ice_restart_column.F90
@@ -1065,9 +1065,9 @@
       ! Zero out tracers over land
       !-----------------------------------------------------------------
 
-      !$OMP PARALLEL DO PRIVATE(iblk,i,j)
+      !$OMP PARALLEL DO PRIVATE(iblk,i,j,ilo,ihi,jlo,jhi,this_block)
       do iblk = 1, nblocks
-         this_block = get_block(blocks_ice(iblk),iblk)         
+         this_block = get_block(blocks_ice(iblk),iblk)
          ilo = this_block%ilo
          ihi = this_block%ihi
          jlo = this_block%jlo
